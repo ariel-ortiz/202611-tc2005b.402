452aa62fbb01d9352a9e772f3450c615c4ff55e2
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-03-04 18:58:12 +0000

    Add code for fibonacci endpoint

--- 4. Express/ejemplo1/app.mjs
+++ 4. Express/ejemplo1/app.mjs
@@ -11,9 +11,65 @@ app.get('/', (req, res) => {
   });
 });
 
+const monthNames = [
+  'enero',
+  'febrero',
+  'marzo',
+  'abril',
+  'mayo',
+  'junio',
+  'julio',
+  'agosto',
+  'septiembre',
+  'octubre',
+  'noviembre',
+  'diciembre'
+];
+
+const weekDayNames = [
+  'domingo',
+  'lunes',
+  'martes',
+  'miércoles',
+  'jueves',
+  'viernes',
+  'sábado'
+];
+
 app.get('/fecha', (req, res) => {
   const rightNow = new Date();
-  res.json(rightNow.toString());
+  res.json({
+    year: rightNow.getFullYear(),
+    month: monthNames[rightNow.getMonth()],
+    day: rightNow.getDate(),
+    dayOfWeek: weekDayNames[rightNow.getDay()],
+    hour: rightNow.getHours(),
+    minute: rightNow.getMinutes(),
+    second: rightNow.getSeconds(),
+    feeling: '\u263a\u03A8😆'
+  });
+});
+
+function fibo(n) {
+  const result = [0, 1];
+  while (result.length < n) {
+    result.push(result.at(-1) + result.at(-2));
+  }
+  return result.slice(0, n);
+}
+
+app.get('/fibonacci/:num', (req, res) => {
+  const num = parseInt(req.params.num) || 0;
+  if (num < 0) {
+    res.status(400).json({
+      message: `WTF! Can't work with negative numbers: ${ num }`
+    });
+  } else {
+    res.json({
+      n: num,
+      values: fibo(num)
+    });
+  }
 });
 
 app.listen(port, () => {
